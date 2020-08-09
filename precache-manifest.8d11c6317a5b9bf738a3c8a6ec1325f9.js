52f73c4da220f7aeada71f3593622f29e0672dea
Nikhil Singh Gautam <nick9124>
2020-08-10 02:07:04 +0530

    Updates

---
+++ precache-manifest.8d11c6317a5b9bf738a3c8a6ec1325f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d215d4b0082589ae9c10491273627a5d",
+    "url": "/robo-monsters/index.html"
+  },
+  {
+    "revision": "2fe683b13f5a84faa053",
+    "url": "/robo-monsters/static/css/main.afc1ee9f.chunk.css"
+  },
+  {
+    "revision": "3871fb7ea1ca376ffe72",
+    "url": "/robo-monsters/static/js/2.fd02da04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-monsters/static/js/2.fd02da04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe683b13f5a84faa053",
+    "url": "/robo-monsters/static/js/main.f05295e1.chunk.js"
+  },
+  {
+    "revision": "58d5e425c9162cb930da",
+    "url": "/robo-monsters/static/js/runtime-main.0c572125.js"
+  }
+]);
